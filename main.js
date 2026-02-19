a024af9c87ded3602137066bbfaf458af3e86b24
A-Hughes03 <adamhughesme1@gmail.com>
2026-02-19 00:45:06 -0500

    add Chunk class and integrate rendering in main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Player } from "./player.js";
+import { Chunk } from "./chunk.js";
 import { setupInput } from "./input.js";
 
 const canvas = document.getElementById('gameCanvas');
@@ -16,10 +17,16 @@ function resizeCanvas() {
 window.addEventListener('resize', resizeCanvas);
 resizeCanvas();
 
+const testChunk = new Chunk(0, 0, 32, 16);
+
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    testChunk.render(ctx, 0, 0);
+
     player.updatePosition();
     player.render(ctx);
+
     requestAnimationFrame(gameLoop);
 }
 
